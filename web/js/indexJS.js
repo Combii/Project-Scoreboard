97c54a69ae4c4c47f9b82ee0e81d090a0c85192a
Combii <davidstovle@gmail.com>
2017-05-11 14:32:39 +0200

    Backup

--- web/js/indexJS.js
+++ web/js/indexJS.js
@@ -3,16 +3,69 @@
  */
 
 
-var score = 0;
+var score1 = 0;
+var score2 = 0;
 
-function setScore(){
-    score = score + 1;
+window.onload = function() {
+    var canvas1 = document.getElementById("score1");
+    var canvas2 = document.getElementById("score2");
+
+    var ctx1 = canvas1.getContext("2d");
+    var ctx2 = canvas2.getContext("2d");
+
+    ctx1.fillStyle = "white";
+    ctx1.font = "100px Arial";
+    ctx1.textAlign = "center";
+
+    ctx2.fillStyle = "white";
+    ctx2.font = "100px Arial";
+    ctx2.textAlign = "center";
+
+    ctx1.fillText("0", canvas1.width / 2, canvas1.height / 2);
+    ctx2.fillText("0", canvas2.width / 2, canvas2.height / 2);
+};
+
+function setScore1(){
+    score1 = score1 + 1;
+
     var canvas = document.getElementById("score1");
     var ctx = canvas.getContext("2d");
 
-    ctx.clearRect(10, 10, canvas.width, canvas.height);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    ctx.font = "30px Arial";
-    ctx.fillText(score,10,50);
+    ctx.fillText(score1, canvas.width/2, canvas.height/2);
 }
 
+function setScore2(){
+    score2 = score2 + 1;
+
+    var canvas = document.getElementById("score2");
+    var ctx = canvas.getContext("2d");
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    ctx.fillText(score2, canvas.width/2, canvas.height/2);
+}
+
+function clearScore() {
+    var canvas1 = document.getElementById("score1");
+    var canvas2 = document.getElementById("score2");
+
+    var ctx1 = canvas1.getContext("2d");
+    var ctx2 = canvas2.getContext("2d");
+
+    ctx1.clearRect(0, 0, canvas1.width, canvas1.height);
+    ctx2.clearRect(0, 0, canvas2.width, canvas2.height);
+
+    ctx1.fillText("0", canvas1.width / 2, canvas1.height / 2);
+    ctx2.fillText("0", canvas2.width / 2, canvas2.height / 2);
+}
+
+function timer(){
+    var timer = new Timer();
+    timer.start();
+    timer.addEventListener('secondsUpdated', function (e) {
+        $('#basicUsage').html(timer.getTimeValues().toString());
+    });
+
+}
